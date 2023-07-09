dc011f29634b7c05fd1cc6473e56412a003bd072
exwizzar <exwizzar@gmail.com>
2023-07-09 12:17:01 +1000

    removed the extra imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,3 @@
-import React, {useEffect, useState} from 'react';
-import { getFilmList } from './store/action-creators/fiim';
-import { useTypedSelector } from './hooks/useTypedSelector';
 import List from './components/List';
 
 
