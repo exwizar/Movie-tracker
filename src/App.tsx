b4e9f6b43916a727e26cf34347750f16efbb7211
exwizzar <exwizzar@gmail.com>
2023-07-21 06:49:36 +1000

    Added title and styles

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,15 @@ function App() {
 
   return (
     <div className='app'>
+      <div className='app-head'>
+        <h1 className='app-head__heading'>250 лучших фильмов</h1>
+        <p className='app-head__info'>Рейтинг составлен по результатам голосования посетителей сайта. Любой желающий может принять в нем участие, проголосовав за свой любимый фильм.</p>
+        <a 
+          target='_blank'
+          className='app-head__link'
+          href='https://yandex.ru/support/kinopoisk/rating.html#rating__rating-2_1'
+        >Как формируется рейтинг</a>
+      </div>
       <div>
         <List/>
       </div>
