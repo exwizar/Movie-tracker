4bf072c712f837b61d2cc96fdf92e6c387af4864
exwizzar <exwizzar@gmail.com>
2023-07-21 06:19:02 +1000

    Created a block with information about movies and added styles

--- src/components/filmCard/filmCard.tsx
+++ src/components/filmCard/filmCard.tsx
@@ -7,10 +7,30 @@ interface FilmProps {
 }
 
 const FilmCard: React.FC<FilmProps> = ({film}) => {
+  let countries = film.countries.map(item => item.country)
+
 
   return (
     <div className='card'>
-       <img className='card-img' src={film.posterUrlPreview} alt="aaa" />
+
+        
+        <div className="card-block">
+        <img className='card-img' src={film.posterUrlPreview} alt="aaa" />
+        <div className='card-info'>
+          <h3 className='card-info__name'>{film.nameRu}</h3>
+          <p className='card-info__about'>{film.nameEn}, {film.year}, {film.filmLength} мин</p>
+          <div className='card-info__countries'>{countries.join(', ')}</div>
+        </div>
+        </div>
+
+        <div className='card-menu'>
+
+            <span className='card-menu__span item1'></span>
+                <p className='card-menu__rating'>{film.rating}</p>
+            <span className='card-menu__span item2'></span>
+          
+            <button className='card-menu__btn'>Буду смотреть</button>
+        </div>
     </div>
   )
 }
