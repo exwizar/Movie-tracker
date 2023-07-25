7b5a91952b6807400a1c9ec643739950dc445a01
exwizzar <exwizzar@gmail.com>
2023-07-25 11:18:01 +1000

    added movieBasketReducer

--- src/store/reducers/index.ts
+++ src/store/reducers/index.ts
@@ -1,9 +1,11 @@
 import {combineReducers} from 'redux'
 import { filmReducer } from './filmReducer'
+import { MovieBasketReducer } from './movieBasketReducer'
 
 
 export const rootReducer = combineReducers({
-    film: filmReducer
+    film: filmReducer,
+    movie: MovieBasketReducer
 })
 
 export type RootState = ReturnType<typeof rootReducer>
