4c4f74b4c5cf1a35d60fd11ca30f53cac099193d
exwizzar <exwizzar@gmail.com>
2023-07-25 10:39:17 +1000

    created a movieBasket

---
+++ src/components/movieBasket/MovieBasket.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+interface Props {
+    counter: number
+}
+
+const MovieBasket = (counter: Props) => {
+  return (
+    <div></div>
+  )
+}
+
+export default MovieBasket
