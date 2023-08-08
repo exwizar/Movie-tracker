7662b9e694b36efcc6cf44bf63d12e78597a5814
exwizzar <exwizzar@gmail.com>
2023-08-08 17:11:34 +1000

    created movieId

--- src/interface/film.ts
+++ src/interface/film.ts
@@ -1,5 +1,5 @@
 export interface IMovieBasket {
-    movie: IFilm[]
+    movie: IFilm[];
     error: null | string;
 }
 
--- src/store/reducers/movieBasketReducer.ts
+++ src/store/reducers/movieBasketReducer.ts
@@ -1,11 +1,14 @@
 import { IMovieBasket } from "../../interface/film"
 export const initialState: IMovieBasket = {
-    movie: localStorage.getItem('movie') ? JSON.parse(localStorage.getItem('movie') || '[]') : []
+    movie: localStorage.getItem('movie') ? JSON.parse(localStorage.getItem('movie') || '[]') : [],
+    error: null
 }
 export const MovieBasketReducer = (state = initialState, action: any): IMovieBasket => {
     switch(action.type) {
         case "MOVIE_ADD":
             return {...state,  movie: [...state.movie, action.payload]}
+        case "MOVIE_ERROR":
+            return {...state, error: action.payload, movie: []}
         default:
             return state
     }
