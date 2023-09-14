0e048de1d8b6b76b31fe9ce8cc9636f689114ff1
exwizzar <exwizzar@gmail.com>
2023-09-14 19:59:48 +1000

    added modal in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import List from './components/List';
 import './app.scss'
+import Modal from './components/ModalSaveMovie/Modal';
 
 function App() {
 
 
   return (
     <div className='app'>
+      <Modal/>
       <div className='app-head'>
         <h1 className='app-head__heading'>250 лучших фильмов</h1>
         <p className='app-head__info'>Рейтинг составлен по результатам голосования посетителей сайта. Любой желающий может принять в нем участие, проголосовав за свой любимый фильм.</p>
