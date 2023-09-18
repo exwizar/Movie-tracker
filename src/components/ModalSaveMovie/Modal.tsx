242d46d61169e6c1d5cbafbdf5a3540a28b08a12
exwizzar <exwizzar@gmail.com>
2023-09-18 15:49:19 +1000

    added styles for modal

--- src/components/ModalSaveMovie/Modal.tsx
+++ src/components/ModalSaveMovie/Modal.tsx
@@ -7,14 +7,33 @@ import { useDispatch } from 'react-redux'
 const Modal = () => {
   const dispatch = useDispatch();
   const {active} = useTypedSelector(state => state.active)
+  const {bookmarkWithFilms} = useTypedSelector(state => state.bookmark)
 
-  console.log(active)
+  console.log(bookmarkWithFilms)
 
   return (
     <div className={active ? "modal active" : "modal"} onClick={() => dispatch({type:"MODAL_ACTIVE", payload: !active})}>
     
       <div className={active ?'modal__content active' : "modal__content"} onClick={e => e.stopPropagation()}>
-        fsd
+
+        {bookmarkWithFilms?.map((film: any) => 
+          <div className='film'>
+            <div className="film-block">
+            <img className='film-img' style={{width: '100px'}} src={film.posterUrlPreview} alt="aaa" />
+              <div className='film-info'>
+                <h3 className='film-info__name'>{film.nameRu}</h3>
+                <p className='film-info__about'>{film.nameOriginal} {film.year}, {film.filmLength} мин</p>
+                <p className='film-menu__rating'> Рейтинг: {film.ratingKinopoisk}</p>
+              </div>
+            </div>
+
+            <div className='film-delete'>
+                <button className='film-delete__btn'>Удалить <span className='basket'></span></button>
+            </div>
+          </div>
+
+        )}
+
       </div>
     </div>
   )
