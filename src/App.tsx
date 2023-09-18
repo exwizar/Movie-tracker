ebb191f81a7657fe16d5ab3b8cd04ebaba76b5ad
exwizzar <exwizzar@gmail.com>
2023-09-18 18:21:44 +1000

    added style for button

--- src/App.tsx
+++ src/App.tsx
@@ -11,17 +11,28 @@ function App() {
 
   return (
     <div className='app'>
-      <button onClick={() => dispatch({type:"MODAL_ACTIVE", payload: !active})}>Open modal</button>
+
       <Modal/>
-      <div className='app-head'>
-        <h1 className='app-head__heading'>250 лучших фильмов</h1>
-        <p className='app-head__info'>Рейтинг составлен по результатам голосования посетителей сайта. Любой желающий может принять в нем участие, проголосовав за свой любимый фильм.</p>
-        <a 
-          target='_blank'
-          className='app-head__link'
-          href='https://yandex.ru/support/kinopoisk/rating.html#rating__rating-2_1'
-        >Как формируется рейтинг</a>
-      </div>
+
+        <div className='app-head'>
+
+          <div className='app-main'>
+            <h1 className='app-main__heading'>250 лучших фильмов</h1>
+            <p className='app-main__info'>Рейтинг составлен по результатам голосования посетителей сайта. Любой желающий может принять в нем участие, проголосовав за свой любимый фильм.</p>
+            <a 
+              target='_blank'
+              className='app-main__link'
+              href='https://yandex.ru/support/kinopoisk/rating.html#rating__rating-2_1'
+              >Как формируется рейтинг
+            </a>
+          </div>
+          
+          <div className='app-modal'>
+            <button className='app-modal__open' onClick={() => dispatch({type:"MODAL_ACTIVE", payload: !active})}></button>
+          </div>
+          
+        </div>
+
       <div>
         <List/>
       </div>
