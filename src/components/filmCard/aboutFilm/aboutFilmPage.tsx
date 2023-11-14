72c9e6fcf4345740f18a4a2b10e2fedf2b32b7a2
Nikitos <C:\Users\iljushka\AppData\Roaming\The Bat! Pwd>
2023-11-14 16:43:22 +1000

    added styles

--- src/components/filmCard/aboutFilm/aboutFilmPage.tsx
+++ src/components/filmCard/aboutFilm/aboutFilmPage.tsx
@@ -22,27 +22,27 @@ const AboutFilmPage = () => {
       .then(res => setPost(res))
 
 
-        fetch(`https://kinopoiskapiunofficial.tech/api/v2.2/films/${id}/box_office`, {
-        method: 'GET',
-        headers: {
-          'X-API-KEY': '89816edc-48ec-4f94-b2ee-0e59946b6d75',
-          'Content-Type': 'application/json',
-        },
-      })
-        .then(res => res.json())
-        .then(res => setMoney(res))
+    fetch(`https://kinopoiskapiunofficial.tech/api/v2.2/films/${id}/box_office`, {
+      method: 'GET',
+      headers: {
+        'X-API-KEY': '89816edc-48ec-4f94-b2ee-0e59946b6d75',
+        'Content-Type': 'application/json',
+      },
+    })
+      .then(res => res.json())
+      .then(res => setMoney(res))
 
-        fetch(`https://kinopoiskapiunofficial.tech/api/v1/staff?filmId=${id}`, {
-          method: 'GET',
-          headers: {
-            'X-API-KEY': '89816edc-48ec-4f94-b2ee-0e59946b6d75',
-            'Content-Type': 'application/json',
-          },
-        })
-          .then(res => res.json())
-          .then(res => setStaff(res))
+    fetch(`https://kinopoiskapiunofficial.tech/api/v1/staff?filmId=${id}`, {
+      method: 'GET',
+      headers: {
+        'X-API-KEY': '89816edc-48ec-4f94-b2ee-0e59946b6d75',
+        'Content-Type': 'application/json',
+      },
+    })
+      .then(res => res.json())
+      .then(res => setStaff(res))
   }, [id])
-  console.log(money)
+  console.log(post)
 
   return (
     <div className='post'>
@@ -53,73 +53,82 @@ const AboutFilmPage = () => {
       <div className="wrapper">
 
         <div className="main">
-        <div className="header">
-          <h1>{post.nameRu} ({post.year})</h1>
-          <div>{post.nameOriginal} +{post.ratingAgeLimits?.split('').filter((item: any) => Number(item)).join('')}</div>
-        </div>
-           <h2>О фильме</h2>
-           <div className="styles_rowDark">
+          <div className="title">
+            <div className='title-nameRu'>{post.nameRu} ({post.year})</div>
+            <div className='title-nameEn'>{post.nameOriginal} +{post.ratingAgeLimits?.split('').filter((item: any) => Number(item)).join('')}</div>
+          </div>
+          <h2 className='title-info'>О фильме</h2>
+          <div className="styles_column">
+
+            <div className="styles_rowDark">
               <div className="styles_titleDark">Страна</div>
-              <div className="styles_valueDark">{post.countries?.map((item: any) => item.country)}</div>
-           </div>
-           <div className="styles_rowDark">
+              <div className="styles_valueDark">{post.countries?.map((item: any) => item.country).join(', ')}</div>
+            </div>
+            <div className="styles_rowDark">
               <div className="styles_titleDark">Жанр</div>
               <div className="styles_valueDark">{post.genres?.map((item: any) => item.genre).join(', ')}</div>
-           </div>
+            </div>
 
-           <div className="styles_rowDark">
+            <div className="styles_rowDark">
               <div className="styles_titleDark">Слоган</div>
               <div className="styles_valueDark">{post.slogan}</div>
-           </div>
+            </div>
 
-           <div className="styles_rowDark">
+            <div className="styles_rowDark">
               <div className="styles_titleDark">Режиссёр</div>
               <div className="styles_valueDark">{staff[0]?.nameRu}</div>
-           </div>
+            </div>
 
-           <div className="styles_rowDark">
+            <div className="styles_rowDark">
               <div className="styles_titleDark">Сценарист</div>
-              <div className="styles_valueDark">{staff?.map((item: any) => item.professionText === "Сценаристы" ? `${item.nameRu},` : '' ).join('')?.slice(0, -1)}</div>
-           </div>
+              <div className="styles_valueDark">{staff?.map((item: any) => item.professionText === "Сценаристы" ? `${item.nameRu},` : '').join(' ')?.slice(0, -1)}</div>
+            </div>
 
-           <div className="styles_rowDark">
+            <div className="styles_rowDark">
               <div className="styles_titleDark">Продюсеры</div>
-              <div className="styles_valueDark">{staff?.map((item: any) => item.professionText === "Продюсеры" ? `${item.nameRu},` : '' ).join('')?.slice(0, -1)}</div>
-           </div>
+              <div className="styles_valueDark">{staff?.map((item: any) => item.professionText === "Продюсеры" ? `${item.nameRu},` : '').join(' ')?.slice(0, -1)}</div>
+            </div>
 
-           <div className="styles_rowDark">
+            <div className="styles_rowDark">
               <div className="styles_titleDark">Художники</div>
-              <div className="styles_valueDark">{staff?.map((item: any) => item.professionText === "Художники" ? `${item.nameRu},` : '' ).join('')?.slice(0, -1)}</div>
-           </div>
+              <div className="styles_valueDark">{staff?.map((item: any) => item.professionText === "Художники" ? `${item.nameRu},` : '').join(' ')?.slice(0, -1)}</div>
+            </div>
 
-           <div className="styles_rowDark">
+            <div className="styles_rowDark">
               <div className="styles_titleDark">Монтажеры</div>
-              <div className="styles_valueDark">{staff?.map((item: any) => item.professionText === "Монтажеры" ? `${item.nameRu},` : '' ).join('')?.slice(0, -1)}</div>
-           </div>
+              <div className="styles_valueDark">{staff?.map((item: any) => item.professionText === "Монтажеры" ? `${item.nameRu},` : '').join(' ')?.slice(0, -1)}</div>
+            </div>
 
-           
-           <div className="styles_rowDark">
+
+            <div className="styles_rowDark">
               <div className="styles_titleDark">Бюджет</div>
               <div className="styles_valueDark">${money.items?.map((item: any) => item.type === "BUDGET" ? item.amount : '')}</div>
-           </div>
+            </div>
 
-           
-           <div className="styles_rowDark">
+
+            <div className="styles_rowDark">
               <div className="styles_titleDark">Сборы в США</div>
               <div className="styles_valueDark">${money.items?.map((item: any) => item.type === "USA" ? item.amount : '')}</div>
-           </div>
+            </div>
 
-           
-           <div className="styles_rowDark">
+
+            <div className="styles_rowDark">
               <div className="styles_titleDark">Сборы в мире</div>
               <div className="styles_valueDark">${money.items?.map((item: any) => item.type === "WORLD" ? item.amount : '')}</div>
-           </div>
+            </div>
+
+            <div className="styles_rowDark">
+              <div className="styles_titleDark">Время</div>
+              <div className="styles_valueDark">{post.filmLength} мин. / {`${Math.floor(post.filmLength/60)}:${Math.floor(post.filmLength%60)}`}</div>
+            </div>
+
+          </div>
 
         </div>
         <div className="raiting">
-          99
+          {post.ratingKinopoisk}
+          
         </div>
-
       </div>
     </div>
   )
